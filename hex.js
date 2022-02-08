63bc42af3f5af5a81d0481ef497503f531f8a148
Batuhan <yesiltas_sibel@hotmail.com>
2022-02-08 17:56:19 +0300

    js

---
+++ hex.js
@@ -0,0 +1,17 @@
+let hex =[0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+
+let color = document.querySelector(".color");
+let btn= document.getElementById("btn");
+
+btn.onclick=()=>{
+   let code=`#`;
+   for(let i=0; i<6;i++){
+       code+=hex[getRandomNumber()]
+   }
+//    console.log(code);
+document.body.style.backgroundColor=code
+color.innerText=code
+
+}
+
+ const getRandomNumber = ()  => Math.floor(Math.random()*hex.length);
